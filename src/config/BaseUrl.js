98cda87e84a9efb6b618257a4130245fc9988486
kalai <kalai@cdp360.com>
2024-06-03 10:23:55 +0530

    new code push

--- src/config/BaseUrl.js
+++ src/config/BaseUrl.js
@@ -3,18 +3,31 @@ import axios from "axios";
 const instanceBaseUrl=axios.create({
     baseURL:""
 })
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
-  }, function (error) {
+  },
+  function (error) {
     return Promise.reject(error);
-  });
+  }
+);
 
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(
+  function (response) {
     return response;
-  }, function (error) {
-   
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      console.error('Unauthorized, redirecting to login...');
+      window.location = '/login';
+    }
     return Promise.reject(error);
-  });
+  }
+);
 
 
   export default instanceBaseUrl;
