168753dbe365f1a1bdf48398b69827c773500f26
kalai <kalai@cdp360.com>
2024-06-24 17:53:01 +0530

    new folder env changes push

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
-import useAxios from 'config/AxiosAnotherMethod';
 import React, { useEffect } from 'react'
-
+import useAxios from '../../config/AxiosAnotherMethod';
+import { EnvLables } from './../../middleware/EnvDatas/EnvLabals';
 function Home() {
   const { response, error, loading, fetchData } = useAxios();
   const getData=async()=>{
@@ -12,7 +12,7 @@ function Home() {
 
   return (
     <div>Home {error} {loading?"...Loading":null}
-    
+    {EnvLables.BASE_URL_DEVELOPMENT}
     <div>
       {response?.map((item,index)=>{
         return(
--- src/middleware/EnvDatas/EnvLabals.js
+++ src/middleware/EnvDatas/EnvLabals.js
@@ -1,6 +1,7 @@
 
 
 export const EnvLables={
-    "BASE_URL":"http://localhost:3000/base_url",
+    "BASE_URL_DEVELOPMENT":process.env.REACT_APP_BASE_URL_DEVELOPMENT,
+    "BASE_URL_PRODUCTION":process.env.REACT_APP_BASE_URL_PRODUCTION,
     "PORT":process.env.PORT
 }
